9b0afa3ed98fcbe7ffb0f11256bb49514878a01c
MinseoKim-dev <msgn1728@naver.com>
2022-05-06 21:45:57 +0900

    init

---
+++ src/main/java/com/mskim/mintacritic/MintaCriticApplication.java
@@ -0,0 +1,13 @@
+package com.mskim.mintacritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintaCriticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintaCriticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mskim/mintacritic/MintaCriticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mskim.mintacritic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintaCriticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
